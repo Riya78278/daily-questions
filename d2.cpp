b28c5247a30ade8fd3781529f26e89ffbae24278
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-18 15:46:12 +0530

    Add files via upload

---
+++ d2.cpp
@@ -0,0 +1,19 @@
+// 717. 1-bit and 2-bit Characters
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int i = 0;
+        int n = bits.size();
+        while (i < n - 1) {
+            if (bits[i] == 0) {
+                i += 1;  
+            } else {
+                i += 2;  
+            }
+        }
+        return i == n - 1;  // true if last is single-bit
+    }
+};
