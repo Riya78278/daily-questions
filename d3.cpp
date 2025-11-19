e1218f899972fdcef9d1a7526f0d7cdf83fb44fa
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-19 18:58:01 +0530

    Add files via upload

---
+++ d3.cpp
@@ -0,0 +1,20 @@
+// 2154. Keep Multiplying Found Values by Two
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==original){
+                original=nums[i]*2;
+            }
+            if(nums[i] != original && nums[i]> original){
+                break;
+            }
+        }
+        return original;
+    }
+};
