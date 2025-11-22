18cf18982a9726bd0476aee97c61a788d0b6b872
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-22 13:15:51 +0530

    Add files via upload

---
+++ d8.cpp
@@ -0,0 +1,43 @@
+// Parenthesis Checker
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<char> st;
+        
+        for(int i = 0; i < s.size(); i++ ) {
+
+            if(s.size() == 1 && (s[i] == ']' || s[i] == '}' || s[i] == ')')) {
+                return false;
+            }
+
+            if(s[i] == '[' || s[i] == '{' || s[i] == '(') {
+                st.push(s[i]);
+            }
+            else if(s[i] == ']' || s[i] == '}' || s[i] == ')') {
+
+                // *** FIX: check empty before top() ***
+                if(st.empty()) return false;
+
+                if((s[i] == ')' && st.top() == '(') ||
+                   (s[i] == '}' && st.top() == '{') ||
+                   (s[i] == ']' && st.top() == '[')) {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        
+        if(!st.empty()) {
+            return false;
+        }
+        return true;
+    }
+};
