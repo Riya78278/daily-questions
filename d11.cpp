35b0e34c6d8232699ee575f8331cec4021794a96
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-24 22:08:17 +0530

    Add files via upload

---
+++ d11.cpp
@@ -0,0 +1,18 @@
+// Binary Prefix Divisible By 5
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        int n=nums.size();
+        int remainder=0;
+        vector<bool>ans;
+        ans.reserve(n);
+        for(int i=0;i<n;i++){
+            remainder=(remainder*2 +nums[i])%5;
+            ans.push_back(remainder==0);
+        }
+        return ans;
+    }
+};
