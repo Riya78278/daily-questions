f894959a4b58faaaa10c784369f3632f5d72bce6
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-25 23:43:44 +0530

    Add files via upload

---
+++ d13.cpp
@@ -0,0 +1,21 @@
+// 1015 Smallest Integer Divisible by K 
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k==1){
+            return 1;
+        }
+        int num=0;
+        for(int i=1;i<=k;i++){
+            num=(num*10+1)%k;
+
+            if(num==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
